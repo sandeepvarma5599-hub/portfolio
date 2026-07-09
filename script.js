8ae639b36d721b7dd17946f4c547c033e5175347
sandeepvarma5599-hub <sandeepvarma5599@gmail.com>
2026-07-09 17:16:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.style.background = '#e2e8f0';
-    } else {
-        navbar.style.background = '#ffffff';
-    }
-});
